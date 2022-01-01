0811a46f0bbd5217c6ab772a48b50dfadbe66f9c
Tran Bin <binditto@MBP-cua-Tran.hsd1.ca.comcast.net>
2022-01-01 03:13:04 -0800

    updating our app with a shop page, colleciton preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path='/' element={<HomePage/>}/>
-        <Route path='shop/hats' element={<HatsPage/>}/>
+        <Route path='/shop' element={<ShopPage/>}/>
       </Routes>
     </div>
   );
